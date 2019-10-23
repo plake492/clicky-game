3a00de8929a2b832933018f4eca03c172c95bfe2
plake <plake492@gmail.com>
2019-10-22 22:55:24 -0400

    Updates

---
+++ precache-manifest.a527ad9a73ed491c83b7bb330aa49c11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d264911ff843a2bcbc1254d9fec7c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3dd28b0d984776736a8",
+    "url": "/static/css/main.c78dc409.chunk.css"
+  },
+  {
+    "revision": "9177c3124b62fd532143",
+    "url": "/static/js/2.42c9ff17.chunk.js"
+  },
+  {
+    "revision": "a3dd28b0d984776736a8",
+    "url": "/static/js/main.270bb989.chunk.js"
+  },
+  {
+    "revision": "1f1e643385e17360f83c",
+    "url": "/static/js/runtime-main.c2d72319.js"
+  }
+]);
